de2248d8281c2aa684fbc7240c59a7501573c742
Shreemathi07 <shreemathielumalai07@gmail.com>
2026-07-15 22:47:58 +0530

    Initial commit - Hospital Appointment Booking System

---
+++ appointment.py
@@ -0,0 +1,8 @@
+print("======================================")
+print(" Hospital Appointment Booking System ")
+print("======================================")
+
+hospital_name = "shree Care Hospital"
+
+print("Welcome to", hospital_name)
+print("Appointment Booking System Started Successfully")
