8ed7012e23f5ebbc102874d07bc82d8928e70855
Shreemathi07 <shreemathielumalai07@gmail.com>
2026-07-15 23:03:36 +0530

    Updated patient registration module

--- appointment.py
+++ appointment.py
@@ -5,4 +5,13 @@ print("======================================")
 hospital_name = "shree Care Hospital"
 
 print("Welcome to", hospital_name)
-print("Appointment Booking System Started Successfully")
+
+patient_name = "John"
+patient_age = 28
+doctor_name = "Dr. Shree"
+
+print("Patient Name :", patient_name)
+print("Patient Age  :", patient_age)
+print("Doctor Name  :", doctor_name)
+
+print("Appointment Booked Successfully")
